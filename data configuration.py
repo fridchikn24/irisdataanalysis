9268c09b0b1134a2800ee1b85a87704a2ab226e5
fridchikn24 <sbabar@utexas.edu>
2018-04-28 01:27:20 -0500

    began work on processing data

---
+++ data configuration.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import NumPy as NumPy
+from pandas import series, dataframe
+
+
+idf = pd.dataframe()
+idf = pd.read_csv('irisdata.csv', header = ['sepal lenth', 'sepal width','petal length', 'petal width', 'iris class'])
+
