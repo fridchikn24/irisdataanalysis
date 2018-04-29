1d07dc3b7641e2f33af9bd0e5e8acb827709df0f
fridchikn24 <sbabar@utexas.edu>
2018-04-29 00:31:14 -0500

    added a four point stats array and separated the types of irises

--- data configuration.py
+++ data configuration.py
@@ -1,7 +1,19 @@
+import pandas as pd
 from pandas import read_csv, DataFrame
 import numpy
 
-idf = DataFrame()
-idf = read_csv('iris data.csv', names = ['sepal length', 'sepal width', 'petal length', 'petal width', 'iris class'])
 
-t = 1
+def iris_get():
+    idf = DataFrame()
+    idf = read_csv('iris data.csv', names = ['sepal length', 'sepal width', 'petal length', 'petal width', 'iris class'])
+    return idf
+
+def data_split(bigframe, string):
+    newdf = bigframe.loc['string']
+    return newdf
+
+def sig_values(frame):
+    sf = pd.Series([frame.mean, frame.std, frame.var, frame.count],
+                   index=['mean','stddev', 'var','count'])
+    return sf
+
